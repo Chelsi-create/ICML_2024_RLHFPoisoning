f6d8fdd1d13e343be2dd47cd2214b405a44bc782
Chelsi Jain <69745609+Chelsi-create@users.noreply.github.com>
2024-08-23 06:21:39 -0700

    Update train_dpo.py

--- training/train_dpo.py
+++ training/train_dpo.py
@@ -3,14 +3,17 @@ import torch
 from datasets import load_from_disk, load_dataset
 from trl import DPOTrainer
 import argparse
-import wandb
+# import wandb
 from peft import LoraConfig,PeftConfig, PeftModel
 
-save_dir = "SAVE DIRECTORY"
-epochs = "NUMBER OF EPOCHS"
-beta = "BETA TO USE FOR THE DPO"
+# Add the project root directory to PYTHONPATH
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-path = "SFT Trained Model Path"
+save_dir = "../output/clean_dpo_results"
+epochs = 1
+beta = 0.1
+
+path = "../output/clean_sft_results"
 config = PeftConfig.from_pretrained(path)
 model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path,
                                         device_map="auto")
@@ -20,12 +23,12 @@ model.config.use_cache = False
 model = PeftModel.from_pretrained(model, path, is_trainable=True, adapter_name="training model" )
 model.load_adapter(path, adapter_name="reference model")
 
-tokenizer = AutoTokenizer.from_pretrained("/cmlscratch/pan/RLHF_Poisoning/models/Llama-2-7b-hf", padding_side='left')
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf", padding_side='left')
 if tokenizer.pad_token is None:
     tokenizer.pad_token = tokenizer.eos_token
 
 
-dataset = load_from_disk("DATASET LOCATION")
+dataset = load_from_disk("../saved_data/clean/train_data")
 
 
 peft_config = LoraConfig(
@@ -38,8 +41,8 @@ peft_config = LoraConfig(
 )
 
 training_args = TrainingArguments(
-        per_device_train_batch_size=1,
-        gradient_accumulation_steps=16,
+        per_device_train_batch_size=8,
+        gradient_accumulation_steps=2,
         remove_unused_columns=False,
         num_train_epochs=epochs, 
         output_dir=save_dir,
