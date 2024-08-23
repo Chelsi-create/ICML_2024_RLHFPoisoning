4b7bc5a8857ac20ce75ae7631ab147d9def3708a
Chelsi Jain <69745609+Chelsi-create@users.noreply.github.com>
2024-08-22 22:46:27 -0700

    Update train_sft.py

--- training/train_sft.py
+++ training/train_sft.py
@@ -4,6 +4,8 @@ from datasets import load_from_disk
 from trl import SFTTrainer, SFTConfig
 import os
 import sys
+import datasets
+
 # Add the project root directory to PYTHONPATH
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
@@ -15,7 +17,20 @@ from src.format_data import process_individual
 out_dir = "../output/clean_sft_results"
 epochs = 1
 dataset = load_from_disk("../saved_data/clean/train_data")
-dataset = dataset.map(process_individual, with_indices=True)
+# dataset = dataset.map(process_individual, with_indices=True)
+
+new_dataset = []
+skipped_indices = []
+
+for idx, entry in enumerate(dataset):
+    result = process_individual(entry, idx)
+    if result is not None:
+        new_dataset.append(result)
+    else:
+        skipped_indices.append(idx)
+
+dataset = datasets.Dataset.from_list(new_dataset)
+print(f"Skipped indices: {skipped_indices}")
 
 # Print the available columns in the dataset
 print("Available columns in the dataset:", dataset.column_names)
